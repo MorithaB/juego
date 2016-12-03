3a9d9e7454c78abea4b0b206862c5055adfdb4a9
Johanna <maoritharodriguez@gmail.com>
2016-12-02 20:06:40 -0500

    Funcionalidad básica del juego implementada

--- juego.js
+++ juego.js
@@ -1,74 +1,55 @@
-let color = ['blue', 'red', 'purple', 'amber', 'green',
-    'pink', 'teal', 'yellow']
+'use strict'
+
+let vacio = new Event('vacio')
 
 let celdas = document.querySelectorAll('.celda')
 let opcion = document.querySelector('#opcion')
 
+let tablero = new Tablero(celdas)
+let gestorFichas = new GestorFichas(celdas.length)
+
+tablero.colorearCeldas()
+gestorFichas.colorearFichas()
+
+opcion.appendChild(gestorFichas.entregarFicha())
+
 let drake = dragula({
     accepts: (el, target, source, sibling) => {
+        if (source === target)
+            return false
+
         if (target.dataset.estado === 'lleno')
-            return false;
-        return true;
+            return false
+
+        if(target.classList.contains(el.classList.item(1)))
+            return true
+
+        return false;
     },
 
     moves: (el, source, handle, sibling) => {
-        if(el.dataset.saved)
+        if(el.dataset.saved === 'true')
             return false;
         return true;
     }
 });
 
-for (celda of celdas)
+for (let celda of celdas)
     drake.containers.push(celda)
 
 drake.containers.push(opcion)
 
 drake.on('drop', (el, target, source, sibling) => {
-    el.dataset.saved = true;
-    target.dataset.estado = 'lleno';
-    console.log(el)
-    console.log('dropped into')
-    console.log(target)
+    el.dataset.saved = true
+    target.dataset.estado = 'lleno'
+
+    if(gestorFichas.getNumFichas() > 0)
+        opcion.appendChild(gestorFichas.entregarFicha())
+    else
+        window.dispatchEvent(vacio)
+
 })
 
-
-let elemento = document.createElement('div')
-elemento.classList.add('elemento')
-
-opcion.appendChild(elemento)
-
-let generarIndices = function(size) {
-    let indice = []
-
-    for(let i=0; i<size / 2; i++)
-        indice.push(i, i)
-
-    return indice;
-}
-
-let getRandomInt = function(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-class Tablero {
-    constructor(celdas) {
-        this.celdas = celdas
-    }
-
-    llenarCeldas() {
-        let indice = generarIndices(this.celdas.length)
-
-        for (let celda of this.celdas) {
-            let random = getRandomInt(0, indice.length)
-            let i = indice[random]
-
-            celda.classList.add(color[i])
-            indice.splice(random, 1)
-        }
-    }
-}
-
-let t = new Tablero(celdas)
-t.llenarCeldas()
+window.addEventListener('vacio', (e) => {
+    alert('Buena Buena')
+})
