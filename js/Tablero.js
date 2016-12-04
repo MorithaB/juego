13060921e6955a0429c9678021413860afee5100
Johanna <maoritharodriguez@gmail.com>
2016-12-03 19:29:37 -0500

    Se mejora el algoritmo que colorea las celdas del tablero

--- js/Tablero.js
+++ js/Tablero.js
@@ -10,9 +10,15 @@ class Tablero {
 
     colorearCeldas() {
         let indice = util.generarIndices(this.celdas.length)
+        let ant = -1
 
         for (let celda of this.celdas) {
             let random = util.randomInt(0, indice.length)
+
+            while(ant === indice[random] && indice.length != 1)
+                random = util.randomInt(0, indice.length)
+
+            ant = indice[random]
             let i = indice[random]
 
             celda.classList.add(util.color[i])
